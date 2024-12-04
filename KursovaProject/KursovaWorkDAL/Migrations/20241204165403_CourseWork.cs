3120e40a669c7631a83f59c27912d91478b7a068
Andrian <a.krilyshyn@gmail.com>
2024-12-04 23:41:56 +0200

    initial

---
+++ KursovaProject/KursovaWorkDAL/Migrations/20241204165403_CourseWork.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KursovaWork.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CourseWork : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cars",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    make = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    model = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    year = table.Column<int>(type: "int", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cars", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    first_name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    confirm_password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "car_detail",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    car_id = table.Column<int>(type: "int", nullable: false),
+                    color = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    transmission = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    fuel_type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_car_detail", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_car_detail_cars_car_id",
+                        column: x => x.car_id,
+                        principalTable: "cars",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "car_image",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    car_id = table.Column<int>(type: "int", nullable: false),
+                    url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_car_image", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_car_image_cars_car_id",
+                        column: x => x.car_id,
+                        principalTable: "cars",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "card",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    card_number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    cardholder_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    expiration_month = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    expiration_year = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    cvv = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_card", x => x.user_id);
+                    table.ForeignKey(
+                        name: "FK_card_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    car_id = table.Column<int>(type: "int", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    order_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_order_cars_car_id",
+                        column: x => x.car_id,
+                        principalTable: "cars",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_order_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "configuration_options",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    order_id = table.Column<int>(type: "int", nullable: false),
+                    color = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    transmission = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    fuel_type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_configuration_options", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_configuration_options_order_order_id",
+                        column: x => x.order_id,
+                        principalTable: "order",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_car_detail_car_id",
+                table: "car_detail",
+                column: "car_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_car_image_car_id",
+                table: "car_image",
+                column: "car_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_configuration_options_order_id",
+                table: "configuration_options",
+                column: "order_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_car_id",
+                table: "order",
+                column: "car_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_user_id",
+                table: "order",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "car_detail");
+
+            migrationBuilder.DropTable(
+                name: "car_image");
+
+            migrationBuilder.DropTable(
+                name: "card");
+
+            migrationBuilder.DropTable(
+                name: "configuration_options");
+
+            migrationBuilder.DropTable(
+                name: "order");
+
+            migrationBuilder.DropTable(
+                name: "cars");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
